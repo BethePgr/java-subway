bf5fbf517d630d94ffb8ebf991bccd26dada9e35
jongwonpc <c3392302@naver.com>
2023-05-08 22:39:55 +0900

    chore: 파일 위치 변경

--- src/main/java/subway/domain/line/Line.java
+++ src/main/java/subway/domain/line/Line.java
@@ -1,7 +1,12 @@
-package subway.domain;
+package subway.domain.line;
+
+import java.util.ArrayList;
+import java.util.List;
+import subway.domain.station.Station;
 
 public class Line {
     private String name;
+    private List<Station> stationLists = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
