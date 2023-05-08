e335b95d1ea36e75ce78bf95dc831f935ede10f3
jongwonpc <c3392302@naver.com>
2023-05-08 22:49:09 +0900

    feat: station 초기 설정 완료

---
+++ src/main/java/subway/domain/station/StationService.java
@@ -0,0 +1,22 @@
+package subway.domain.station;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StationService {
+
+    private static String initialStations = "교대역,강남역,역삼역,남부터미널역,양재역,양재시민의숲역,매봉역";
+
+    public StationService(){
+        setInitialStations();
+    }
+    private static void setInitialStations(){
+        List<String> stations = Arrays.stream(initialStations.split(","))
+            .collect(Collectors.toList());
+        for(String station : stations){
+            StationRepository.addStation(new Station(station));
+        }
+    }
+
+}
