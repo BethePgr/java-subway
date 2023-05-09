a4f48d91b70691b0413753c48abfc63fd9b22dc5
SEC <c3392302@naver.com>
2023-05-09 13:06:07 +0900

    feat: stationController 완성

--- src/main/java/subway/domain/station/controller/StationController.java
+++ src/main/java/subway/domain/station/controller/StationController.java
@@ -1,5 +1,48 @@
 package subway.domain.station.controller;
 
+import subway.domain.station.Station;
+import subway.domain.station.service.StationService;
+import subway.domain.station.view.StationOutputView;
+
 public class StationController {
+
+    public static void startStation(){
+        StationOutputView.mainStation();
+        while(true) {
+            String input = StationInputController.startStationInput();
+            if(input.equals("B")){
+                break;
+            }
+            stationFunctions(input);
+        }
+    }
+
+    private static void stationFunctions(String input){
+        addStation(input);
+        deleteStation(input);
+        printAllStations(input);
+    }
+
+    private static void addStation(String input){
+        if(input.equals("1")){
+            String newStation = StationInputController.addStation();
+            StationService.addStation(new Station(newStation));
+            StationOutputView.addedStation();
+        }
+    }
+
+    private static void deleteStation(String input){
+        if(input.equals("2")){
+            String deleteStation = StationInputController.deleteStation();
+            StationService.deleteStation(deleteStation);
+            StationOutputView.deletedStation();
+        }
+    }
+
+    private static void printAllStations(String input){
+        if(input.equals("3")){
+            StationOutputView.printAllStations(StationService.getStations());
+        }
+    }
 
 }
--- src/main/java/subway/domain/station/view/StationOutputView.java
+++ src/main/java/subway/domain/station/view/StationOutputView.java
@@ -5,6 +5,22 @@ import subway.domain.station.Station;
 
 public class StationOutputView {
 
+    public static void mainStation(){
+        System.out.println("## 역 관리 화면\n"
+            + "1. 역 등록\n"
+            + "2. 역 삭제\n"
+            + "3. 역 조회\n"
+            + "B. 돌아가기");
+
+    }
+
+    public static void addedStation(){
+        System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+    }
+
+    public static void deletedStation(){
+        System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
+    }
     public static void addStation(){
         System.out.println("## 등록할 역 이름을 입력하세요.");
     }
@@ -12,7 +28,7 @@ public class StationOutputView {
     public static void printAllStations(List<Station> stations){
         System.out.println("## 역 목록");
         for(Station station : stations){
-            System.out.println("[INFO] " + station);
+            System.out.println("[INFO] " + station.getName());
         }
     }
 
