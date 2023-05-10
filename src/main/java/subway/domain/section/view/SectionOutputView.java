d5f5bfaaf9d0b85ead675b1112a5ea07a9045def
SEC <c3392302@naver.com>
2023-05-10 12:57:12 +0900

    feat: 양식에 맞게 구간관리 기능 처음 시작할때의 출력 문구 작성

---
+++ src/main/java/subway/domain/section/view/SectionOutputView.java
@@ -0,0 +1,11 @@
+package subway.domain.section.view;
+
+public class SectionOutputView {
+
+    public static void sectionMain(){
+        System.out.println("## 구간 관리 화면\n"
+            + "1. 구간 등록\n"
+            + "2. 구간 삭제\n"
+            + "B. 돌아가기");
+    }
+}
