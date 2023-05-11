2f8c5489f290202243f5ab5cd1ebfec6d88d2060
SEC <c3392302@naver.com>
2023-05-11 10:53:08 +0900

    feat: SectionController 구현

---
+++ src/main/java/subway/domain/section/controller/SectionController.java
@@ -0,0 +1,57 @@
+package subway.domain.section.controller;
+
+import subway.domain.line.Line;
+import subway.domain.section.service.SectionService;
+import subway.domain.section.view.SectionOutputView;
+import subway.domain.station.Station;
+
+public class SectionController {
+
+    public static void run(){
+        while(true){
+            String input = SectionInputController.startSectionInput();
+            if(input.equals("B")){
+                break;
+            }
+            sectionFunctions(input);
+        }
+    }
+
+    public static void sectionFunctions(String input){
+        addSection(input);
+        deleteSection(input);
+    }
+
+    public static void addSection(String input){
+        if(input.equals("1")){
+            try{
+                Line line = SectionInputController.sectionInputLine();
+                Station station = SectionInputController.sectionInputStation(line);
+                int num = SectionInputController.sectionInputOrder(line);
+                SectionService.addSection(line,station,num);
+                SectionOutputView.sectionAdded();
+                for(Station stations : line.getStationList()){
+                    System.out.println(stations.getName());
+                }
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void deleteSection(String input){
+        if(input.equals("2")){
+            try{
+                Line line = SectionInputController.sectionInputDeleteLine();
+                Station station = SectionInputController.sectionInputDeleteStation(line);
+                SectionService.deleteSection(line,station);
+                SectionOutputView.sectionDeleted();
+                for(Station stations : line.getStationList()){
+                    System.out.println(stations.getName());
+                }
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
