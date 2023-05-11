af9ab65a1b2ee71dd760abf7fce3e2f2b58833da
jongwonpc <c3392302@naver.com>
2023-05-11 18:41:38 +0900

    refactor: 게임 시작 문구 OutputView에 옮기기

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -9,7 +9,6 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputStart(){
-        System.out.println("## 메인 화면\n1. 역 관리\n2. 노선 관리\n3. 구간 관리\n4. 지하철 노선도 출력\nQ. 종료\n");
         System.out.println("## 원하는 기능을 선택하세요.");
         return scanner.next();
     }
