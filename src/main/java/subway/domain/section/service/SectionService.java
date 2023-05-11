f8ee9918472a2323d0dbd651f751bca05facb768
SEC <c3392302@naver.com>
2023-05-11 09:31:21 +0900

    feat: 섹션 추가 기능

---
+++ src/main/java/subway/domain/section/service/SectionService.java
@@ -0,0 +1,11 @@
+package subway.domain.section.service;
+
+import subway.domain.line.Line;
+import subway.domain.station.Station;
+
+public class SectionService {
+
+    public static void addSection(Line line, Station station,int num){
+        line.getStationList().add(num-1,station);
+    }
+}
