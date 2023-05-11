0ea1d656a0ca6c477a15c090d5f00867777ce303
SEC <c3392302@naver.com>
2023-05-11 10:52:41 +0900

    test: 섹션 추가&삭제 테스트

--- src/test/java/subway/domain/section/service/SectionServiceTest.java
+++ src/test/java/subway/domain/section/service/SectionServiceTest.java
@@ -2,6 +2,7 @@ package subway.domain.section.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import subway.domain.line.LineRepository;
 import subway.domain.section.SectionValidate;
 import subway.domain.station.Station;
 import subway.domain.station.StationInitialization;
+import subway.domain.station.StationRepository;
 
 class SectionServiceTest {
 
@@ -27,13 +29,23 @@ class SectionServiceTest {
     void test1() throws Exception {
 
         Line lineTwo = SectionValidate.validateLineExist("2호선");
-        Station station = SectionValidate.validateStationExist("매봉역");
+        Station station = SectionValidate.validateStationExist(lineTwo,"매봉역");
         int num = 3;
         SectionService.addSection(lineTwo,station,num);
 
-        for(Station stations : lineTwo.getStationList()){
-            System.out.println(stations.getName());
-        }
+        assertEquals(4,lineTwo.getStationList().size());
     }
 
+    @Test
+    @DisplayName("")
+    void test2() throws Exception {
+
+        Line lineTwo = SectionValidate.validateLineExist("2호선");
+        Station station = SectionValidate.validateStationNotExist(lineTwo, "교대역");
+        SectionService.deleteSection(lineTwo,station);
+        assertEquals(2,lineTwo.getStationList().size());
+
+    }
 }
+
+
